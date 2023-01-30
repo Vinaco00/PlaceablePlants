a6e75914fd9c68d54331d509bc2ef913e2204935
vinaco <vennistinaco@gmail.com>
2023-01-29 16:09:50 -0800

    - Added toggle for suppressTreeGrowth in config.yml - Added Bamboo - Fixed bug where Big Dripleaf would not stack correctly - Fixed bug where Big Dripleaf would not retain orientation on place - Added toggle for suppressSugarcaneGrowth in config.yml - Added toggle for suppressBambooGrowth in config.yml

--- README.md
+++ README.md
@@ -3,14 +3,16 @@ Spigot Plugin which allows players to place plants and crops on any surface. Whi
 
 Known Issues
 
-- Big Dripleaf does not stack
-- Big Dripleaf does not retain orientation on place
 - Double sound effects when placed on grass
 - Lilypad places twice on click
+- Plants placed on top of growing crops (wheat, nether wart, etc.) will break on plant growth
+- Sugar Cane and Bamboo break on growth when not on standard block
+- Dripleaf breaks on bonemeal when not on standard block
 
 Planned Features (If I can figure it out)
 
-- Toggle sapling growth suppression
 - Toggle block update suppression
-- Toggle plant growth suppression
+- Toggle plant growth suppression unless on proper block
 - Option for block place whitelist
+- Further update suppression to prevent mass breaking of plants
+- Reload command
--- src/main/java/me/vinaco/placeableplants/PlaceablePlants.java
+++ src/main/java/me/vinaco/placeableplants/PlaceablePlants.java
@@ -4,11 +4,16 @@ import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Bisected;
+import org.bukkit.block.data.Directional;
+import org.bukkit.block.data.Waterlogged;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockGrowEvent;
+import org.bukkit.event.block.BlockSpreadEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.world.StructureGrowEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -17,10 +22,12 @@ import java.util.List;
 
 public final class PlaceablePlants extends JavaPlugin implements Listener {
 
-    ArrayList<String> plantsList = new ArrayList<String>();
-    ArrayList<String> crops = new ArrayList<String>();
-    ArrayList<String> dirts = new ArrayList<String>();
-    ArrayList<String> tall = new ArrayList<String>();
+    ArrayList<String> plantsList = new ArrayList<>();
+    ArrayList<String> crops = new ArrayList<>();
+    ArrayList<String> dirts = new ArrayList<>();
+    ArrayList<String> tall = new ArrayList<>();
+    ArrayList<String> sugarCaneBlocks;
+    ArrayList<String> bambooBlocks;
 
     @Override
     public void onEnable() {
@@ -33,14 +40,18 @@ public final class PlaceablePlants extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlant(PlayerInteractEvent e) {
         Player p = e.getPlayer();
-        if (e.getItem() == null){
+        if (e.getItem() == null) {
             return;
         }
         if (e.getAction() != Action.RIGHT_CLICK_BLOCK) {
             return;
         }
+        p.sendMessage(e.getItem().getType().toString());
         if (plantsList.contains(e.getItem().getType().toString())) {
             BlockFace face = getBlockFace(p);
+            if (face == null){
+                return;
+            }
             Location blockLocation = p.getTargetBlockExact(5).getLocation();
             Material toPlace = e.getItem().getType();
             Location blockBelow = blockLocation.getBlock().getRelative(face).getLocation();
@@ -63,6 +74,14 @@ public final class PlaceablePlants extends JavaPlugin implements Listener {
                     toPlace = Material.CARROTS;
                 } else if (e.getItem().getType().toString().equalsIgnoreCase("SWEET_BERRY")) {
                     toPlace = Material.SWEET_BERRY_BUSH;
+                } else if (e.getItem().getType().toString().equalsIgnoreCase("BAMBOO")) {
+                    toPlace = Material.BAMBOO_SAPLING;
+                }
+            }
+            if (toPlace == Material.BAMBOO_SAPLING) {
+                if (blockBelow.getBlock().getType() == Material.BAMBOO || blockBelow.getBlock().getType() == Material.BAMBOO_SAPLING) {
+                    toPlace = Material.BAMBOO;
+                    blockBelow.getBlock().setType(Material.BAMBOO, false);
                 }
             }
 
@@ -83,6 +102,26 @@ public final class PlaceablePlants extends JavaPlugin implements Listener {
                 blockLocation.getBlock().getRelative(face).setType(toPlace, false);
             }
 
+            if (toPlace == Material.BIG_DRIPLEAF) {
+                Directional direction = (Directional) blockLocation.getBlock().getRelative(face).getBlockData();
+                if (blockBelow.getBlock().getType() == Material.BIG_DRIPLEAF || blockBelow.getBlock().getType() == Material.BIG_DRIPLEAF_STEM) {
+                    BlockFace bellowFacing = ((Directional) blockBelow.getBlock().getBlockData()).getFacing();
+                    direction.setFacing(((Directional) blockBelow.getBlock().getBlockData()).getFacing());
+                    blockBelow.getBlock().setType(Material.BIG_DRIPLEAF_STEM, false);
+
+                    Directional belowDirection = (Directional) blockBelow.getBlock().getBlockData();
+                    System.out.println(bellowFacing);
+                    belowDirection.setFacing(bellowFacing);
+
+                    blockBelow.getBlock().setBlockData(belowDirection);
+                } else {
+                    direction.setFacing(p.getFacing().getOppositeFace());
+                }
+                blockLocation.getBlock().getRelative(face).setBlockData(direction);
+
+
+            }
+
             p.getWorld().playSound(p.getLocation(), Sound.BLOCK_GRASS_PLACE, 3.0F, 1F);
             if (p.getGameMode() != GameMode.CREATIVE) {
                 if (e.getHand() == EquipmentSlot.OFF_HAND) {
@@ -94,17 +133,76 @@ public final class PlaceablePlants extends JavaPlugin implements Listener {
         }
     }
 
+    @EventHandler
+    public void onPlantGrow(BlockGrowEvent e){
+        if (e.getBlock().getRelative(0,-1,0).getType() == Material.SUGAR_CANE){
+            if (!getConfig().getBoolean("options.suppressSugarcaneGrowth")){
+                return;
+            }
+            Block blockBelow = e.getBlock().getRelative(0,-2,0);
+            while (blockBelow.getType() == Material.SUGAR_CANE){
+                blockBelow = blockBelow.getRelative(0,-1,0);
+            }
+            //System.out.println(blockBelow.getType());
+            if (
+                     blockBelow.getRelative(1,0,0).getType() == Material.WATER ||
+                     blockBelow.getRelative(-1,0,0).getType() == Material.WATER ||
+                     blockBelow.getRelative(0,0,1).getType() == Material.WATER ||
+                     blockBelow.getRelative(0,0,-1).getType() == Material.WATER ||
+                     blockBelow.getRelative(1,0,0).getType() == Material.FROSTED_ICE ||
+                     blockBelow.getRelative(-1,0,0).getType() == Material.FROSTED_ICE ||
+                     blockBelow.getRelative(0,0,1).getType() == Material.FROSTED_ICE ||
+                     blockBelow.getRelative(0,0,-1).getType() == Material.FROSTED_ICE ||
+                     blockBelow.getRelative(1,0,0).getBlockData() instanceof Waterlogged ||
+                     blockBelow.getRelative(-1,0,0).getBlockData() instanceof Waterlogged ||
+                     blockBelow.getRelative(0,0,1).getBlockData() instanceof Waterlogged ||
+                     blockBelow.getRelative(0,0,-1).getBlockData() instanceof Waterlogged){
+                if (sugarCaneBlocks.contains(blockBelow.getType().toString())){
+                    return;
+                }
+            }
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onBambooGrow(BlockSpreadEvent e){
+        if (e.getBlock().getRelative(0, -1, 0).getType() == Material.BAMBOO || e.getBlock().getRelative(0,-1,0).getType() == Material.BAMBOO_SAPLING){
+            if (!getConfig().getBoolean("options.suppressBambooGrowth")){
+                return;
+            }
+            Block blockBelow = e.getBlock().getRelative(0,-2,0);
+            while (blockBelow.getType() == Material.BAMBOO || blockBelow.getType() == Material.BAMBOO_SAPLING){
+                blockBelow = blockBelow.getRelative(0,-1,0);
+            }
+            if (bambooBlocks.contains(blockBelow.getType().toString())){
+                return;
+            }
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onTreeGrow(StructureGrowEvent e) {
+        //System.out.println(e.getLocation().getBlock().getRelative(0,-1,0));
+        if (!getConfig().getBoolean("options.suppressTreeGrowth")) {
+            return;
+        }
+        if (!dirts.contains(e.getLocation().getBlock().getRelative(0, -1, 0).getType().toString())) {
+            e.setCancelled(true);
+        }
+    }
+
     /**
      * Initializes the Array Lists obtained from the config folder and defined in the method
-     *
      */
-    public void Plants(){
+    public void Plants() {
         System.out.println("Initialising plants config");
         getConfig().getConfigurationSection("plants").getKeys(false).forEach(plant -> {
             //adds plant to array list if true in config.yml
-            if (getConfig().getBoolean("plants." + plant)){
+            if (getConfig().getBoolean("plants." + plant)) {
                 plantsList.add(plant);
-                //System.out.println("added plant: " + plant);
+                System.out.println("added plant: " + plant);
             }
         });
 
@@ -114,6 +212,7 @@ public final class PlaceablePlants extends JavaPlugin implements Listener {
         crops.add("POTATO");
         crops.add("CARROT");
         crops.add("SWEET_BERRIES");
+        crops.add("BAMBOO");
 
         dirts.add("DIRT");
         dirts.add("GRASS_BLOCK");
@@ -131,6 +230,18 @@ public final class PlaceablePlants extends JavaPlugin implements Listener {
         tall.add("ROSE_BUSH");
         tall.add("PEONY");
 
+        sugarCaneBlocks = (ArrayList<String>)dirts.clone();
+        sugarCaneBlocks.add("SAND");
+        sugarCaneBlocks.add("RED_SAND");
+        sugarCaneBlocks.add("MOSS_BLOCK");
+        sugarCaneBlocks.remove("FARMLAND");
+
+        System.out.println("dirts: " + dirts);
+        System.out.println("sugarcaneblocks " + sugarCaneBlocks);
+
+        bambooBlocks = (ArrayList<String>) sugarCaneBlocks.clone();
+        bambooBlocks.add("GRAVEL");
+
     }
 
     /**
@@ -141,8 +252,8 @@ public final class PlaceablePlants extends JavaPlugin implements Listener {
      */
     public BlockFace getBlockFace(Player player) {
         List<Block> lastTwoTargetBlocks = player.getLastTwoTargetBlocks(null, 100);
-        if (getConfig().getBoolean("options.fullBlocksOnly")){
-        if (lastTwoTargetBlocks.size() != 2 || !lastTwoTargetBlocks.get(1).getType().isOccluding()) return null;
+        if (getConfig().getBoolean("options.fullBlocksOnly")) {
+            if (lastTwoTargetBlocks.size() != 2 || !lastTwoTargetBlocks.get(1).getType().isOccluding()) return null;
         }
         Block targetBlock = lastTwoTargetBlocks.get(1);
         Block adjacentBlock = lastTwoTargetBlocks.get(0);
